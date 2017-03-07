31a099df8c5ee3e7205d546bcb3a722b3a08f7ad
balazsbonis <balazs.bonis@greenfinch.ie>
2017-03-07 10:31:41 +0000

    [GSW-160] - Create Xamarin projects

---
+++ src/KBS.Portals.Calculator/KBS.Portals.Calculator.Android/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace KBS.Portals.Calculator.Droid
+{
+    [Activity(Label = "KBS.Portals.Calculator", Icon = "@drawable/icon", Theme = "@android:style/Theme.Material.Light", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
