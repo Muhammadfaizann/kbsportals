e3bb885b9177f9fe48165ea7562ea9e5fa24b05b
balazsbonis <balazs.bonis@greenfinch.ie>
2017-03-13 14:11:55 +0000

    [KBS-22] - Add CalculatorView to the project

--- src/KBS.Portals.Calculator/KBS.Portals.Calculator/Pages/CalculatorPage.xaml.cs
+++ src/KBS.Portals.Calculator/KBS.Portals.Calculator/Pages/CalculatorPage.xaml.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using CarouselView.FormsPlugin.Abstractions;
+using KBS.Portals.Calculator.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace KBS.Portals.Calculator.Pages
 {
-
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class CalculatorPage : CarouselPage
+    public partial class CalculatorPage : ContentPage
     {
         public CalculatorPage()
         {
             InitializeComponent();
+
+            CalculatorCarousel.ItemsSource = new List<string> {"APR", "IRR", "Rate"};
+            CalculatorCarousel.Position = 0;
         }
     }
 }
