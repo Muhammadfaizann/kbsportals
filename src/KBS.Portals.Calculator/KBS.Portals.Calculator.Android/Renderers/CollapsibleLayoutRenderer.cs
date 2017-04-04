d83837a376a6003157b72037af6ec9a4b69114b9
Robbie Bowman <robbie.bowman@greenfinch.ie>
2017-04-04 11:35:54 +0100

    Simply hide children when title is touched

---
+++ src/KBS.Portals.Calculator/KBS.Portals.Calculator.Android/Renderers/CollapsibleLayoutRenderer.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using KBS.Portals.Calculator.CustomViews;
+using KBS.Portals.Calculator.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+//[assembly: ExportRenderer(typeof(CollapsibleLayout), typeof(CollapsibleLayoutRenderer))]
+namespace KBS.Portals.Calculator.Droid.Renderers
+{
+//    public class CollapsibleLayoutRenderer : ViewRenderer
+//    {
+//
+//    }
+}
---
+++ src/KBS.Portals.Calculator/KBS.Portals.Calculator/CustomViews/CollapsibleLayout.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Windows.Input;
+using AutoMapper.Configuration.Conventions;
+using Xamarin.Forms;
+
+namespace KBS.Portals.Calculator.CustomViews
+{
+    public class CollapsibleLayout : StackLayout
+    {
+        public string Title
+        {
+            get { return _titleLabel.Text; }
+            set { _titleLabel.Text = value; }
+        }
+
+        private readonly Label _titleLabel;
+
+        public CollapsibleLayout()
+        {
+            _titleLabel = new Label
+            {
+                VerticalOptions = LayoutOptions.StartAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                FontSize = 30
+            };
+            Children.Add(_titleLabel);
+            var tapGestureRecognizer = new TapGestureRecognizer {Command = TapCommand};
+            _titleLabel.GestureRecognizers.Add(tapGestureRecognizer);
+        }
+
+        private ICommand TapCommand => new Command(OnTitleClicked);
+
+        public void OnTitleClicked()
+        {
+            Children.ToList().FindAll(v => v != _titleLabel).ForEach(v => v.IsVisible = !v.IsVisible);
+        }
+    }
+}
