12a53cc8acde9c015e828c9e1914d2a5957214da
Robbie Bowman <robbie.bowman@greenfinch.ie>
2017-07-20 15:00:10 +0100

    Add launcher icon, change app name

--- src/KBS.Portals.Calculator/KBS.Portals.Calculator.Android/Properties/AssemblyInfo.cs
+++ src/KBS.Portals.Calculator/KBS.Portals.Calculator.Android/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KBS.Portals.Calculator.Android")]
+[assembly: AssemblyTitle("KBS Calculator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
